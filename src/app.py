429dc58c0b77204b71e9d1076227a19693c1ef7d
Mathews Pious <mathewspious@gmail.com>
2025-12-30 16:17:50 -0800

    updated deplyed env

--- src/app.py
+++ src/app.py
@@ -16,7 +16,7 @@ def info():
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
         'message': 'You are doing great, little human! <3',
-        'deployed_on': 'kubernetes'
+        'deployed_on': 'kind-kubernetes'
     })
 
 @app.route('/api/v1/healthz')
